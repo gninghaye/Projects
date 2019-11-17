fa495b5b7e09b4aba0494b3964910dcefb44a7e7
Gninghaye Malcolmx <gninghayemalcolm@gmail.com>
2019-11-17 14:25:16 +0100

    commit premier

---
+++ GestionEtablissement/GestionEmploiDeTemp/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Android.App;
+
+
+[assembly: AssemblyTitle("GestionEmploiDeTemp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GestionEmploiDeTemp")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
